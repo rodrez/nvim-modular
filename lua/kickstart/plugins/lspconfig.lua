5faf016528c4abcaa77a041bc82743738aae5f15
Fabian Rodriguez <frodriguez@endtrust.com>
2024-05-23 12:22:01 -0500

    Removed ruff-lsp in favor of ruff server

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -173,7 +173,6 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
-        'ruff-lsp',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 

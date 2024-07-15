7c538e2565add6f7abacdb9a41d5ad7cb3517d6f
Rodrez <fabian.rodrez@gmail.com>
2024-07-14 20:20:06 -0500

    added cmake tools

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,9 +1,7 @@
 return {
   'ThePrimeagen/harpoon',
-  branch = 'harpoon2',
   config = function()
     local harpoon = require 'harpoon'
-    harpoon:setup()
 
     vim.keymap.set('n', '<leader>a', function()
       harpoon:list():add()
